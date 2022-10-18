ec95b0c2eac498375f44c22d5628ff2baeb8f2d5
Abner Sasntos <abnersenna@hotmail.com>
2022-10-18 14:11:40 -0300

    iniciado a parte do script

---
+++ src/script.js
@@ -0,0 +1,5 @@
+//imputs//
+const numberInput = document.querySelector('#number-input')
+const nameInput = document.querySelector('#name-input')
+const validityinput = document.querySelector('#validity-input')
+const cvcInput = document.querySelector('#cvc-input')
