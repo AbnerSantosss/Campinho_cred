497b8aa4e8b7e2c3f0eff3afe2678b25fb1eb2e7
Abner Sasntos <abnersenna@hotmail.com>
2022-10-18 16:24:50 -0300

    projeto concluido

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ const myForm = document.querySelector('#my-form')
 //===================CAMPOS-DE-IMPUT===========================//
 const numberInputOne = document.querySelector('#number-input-one')
 const numberInputTwo = document.querySelector('#number-input-two')
-const numberInputThree = document.querySelector('#number-input-Three')
+const numberInputThree = document.querySelector('#number-input-three')
 const numberInputFour = document.querySelector('#number-input-four')
 
 const nameInput = document.querySelector('#name-input')
@@ -19,8 +19,8 @@ const buttonForm = document.querySelector('#submit-button')
 //--Números-impresso-no-Cartão
 const numberCardOne = document.querySelector('.number-card-one')
 const numberCardTwo = document.querySelector('.number-card-two')
-const numberCardTrhee = document.querySelector('.number-card-three')
-const numberCardFour = document.querySelector('.number-card-Four')
+const numberCardThree = document.querySelector('.number-card-three')
+const numberCardFour = document.querySelector('.number-card-four')
 
 //--Nome-titular---//
 const nameCard = document.querySelector('.name-card')
@@ -34,17 +34,49 @@ const cvcCard = document.querySelector('.cvc-card')
 //======================Eventos===========================//
 
 buttonForm.addEventListener('click', event => {
-  alert('olá')
   event.preventDefault()
 
-  const numberValeu = numberInput.value
-  const nameValue = numberInput.value
-  const validityValue = validityinput.value
-  const cvcValue = cvcInput.value
+  const nameValue = nameInput.value
+  const numberValue = numberInputTwo.valeu
 
-  if (numberValeu === '' || nameValue === '') {
-    messageError.textContent = 'Todos os campos são obrigatórios!'
+  if (nameValue === '' || numberValue === '') {
+    messageError.textContent = 'Todos os campos tem que ser preenchidos'
     messageError.classList = 'error-mensage'
-    return
   }
+
+  addContent()
+  clearAll()
 })
+
+//=======FUNÇÕES=======//
+const addContent = () => {
+  numberCardOne.textContent = numberInputOne.value
+  numberCardTwo.textContent = numberInputTwo.value
+  numberCardThree.textContent = numberInputThree.valeu
+  numberCardFour.textContent = numberInputFour.value
+
+  //--Nome-titular---//
+  nameCard.textContent = nameInput.value
+
+  //--Validade---//
+  validityCard.textContent = validityinput.value
+
+  //--CVC-card--//
+  cvcCard.textContent = cvcInput.value
+}
+
+const clearAll = () => {
+  numberInputOne.value = ''
+  numberInputTwo.value = ''
+  numberInputThree.valeu = ''
+  numberInputFour.value = ''
+
+  //--Nome-titular---//
+  nameInput.value = ''
+
+  //--Validade---//
+  validityinput.value = ''
+
+  //--CVC-card--//
+  cvcInput.value = ''
+}
